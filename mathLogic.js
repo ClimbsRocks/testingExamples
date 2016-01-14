a9efc692abebd514551dc3ae53f666eb9a647c07
ClimbsRocks <climbsbytes@gmail.com>
2016-01-14 09:41:29 -0800

    adds some math operations to test

---
+++ mathLogic.js
@@ -0,0 +1,11 @@
+module.exports = {
+  multiply: function(a,b) {
+    return a * b;
+  },
+  cubeRoot: function(input) {
+    return Math.pow(input, 1/3);
+  },
+  remainder: function(a,b) {
+    return a % b;
+  }
+}
